d1396d9b2945907af8513fbabe939b3d61a92315
Frank Corona Prendes <frank.corona@pm.me>
2023-04-01 17:09:24 -0300

    feat(constants/types): add navigation constant and NavItem type definition

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+import { NavItem } from "@/types";
+
+export const navigation: NavItem[] = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Portfolio", path: "/portfolio" },
+  { name: "Contact", path: "/contact" },
+];
