c77d29758addb59d82b68052292758bce3f6b00c
Frank Corona Prendes <frank.corona@pm.me>
2023-04-01 18:00:47 -0300

    feat(page.tsx): add hero section to home page with introduction and arrow icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+import { Raleway } from "next/font/google";
+
+const font = Raleway({ subsets: ["latin"] });
+
 export default function HomePage() {
-  return <main className="my-6 mx-10">Home Page</main>;
+  return (
+    <main
+      className={`${font.className} my-32 mx-auto max-w-4xl leading-tight space-y-8 tracking-widest`}
+    >
+      <h1 className="text-9xl font-extrabold drop-shadow-lg shadow-gray-100">
+        Hello<span className="text-amber-600 text-7xl">.</span>
+      </h1>
+      <div className="flex items-center gap-8 justify-between">
+        <div className="flex gap-8 items-center">
+          <div className="text-9xl font-extrabold ">I am</div>
+          <ul className="list-disc px-2 leading-7 text-md">
+            <li>Computer Science Engineer</li>
+            <li>Software Development Lead</li>
+            <li>Full Stack Developer</li>
+            <li>10+ Years of Experience</li>
+          </ul>
+        </div>
+        <ArrowRightCircleIcon className="h-16 w-16 text-amber-600 hover:cursor-pointer" />
+      </div>
+      <div className="text-9xl font-extrabold ">Frank</div>
+    </main>
+  );
 }
